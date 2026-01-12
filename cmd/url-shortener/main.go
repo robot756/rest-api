73b61a820f7f5a422a59f76e312c461177f60d96
Ivan <shcherbakovivan_2003@mail.ru>
2026-01-12 22:57:36 +0300

    init logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"rest-api/internal/config"
 )
 
 func main() {
 	// TODO: init config: cleanevn
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
 	// TODO: init logger: slog
+	logger := setupLogger(cfg.ENV)
 
-	// TODO: init storage: postreSQL
+	logger.Info("starting url-shortener")
+	logger.Debug("debug message are enable")
+
+	// TODO: init storage: postgreSQL
 
 	// TODO: init router: chi
 
 	// TODO: init run server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case "dev":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	case "prod":
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
